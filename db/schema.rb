2559b9094c6866ceafd2bfb2989e9e458ff989d0
weyewe1 <rajakuraemas@gmail.com>
2015-10-04 15:47:15 +0000

    dummy awesome

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151004151147) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
